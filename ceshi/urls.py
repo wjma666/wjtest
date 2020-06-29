45da84d430e13e283080103a288516f1ce7264e3
wjma666 <jj_mawenjing@163.com>
2020-06-29 23:14:13 +0800

    test_again

--- ceshi/urls.py
+++ ceshi/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 # from django.urls import path
 from django.conf.urls import url
 from app import views
+import re
 
 urlpatterns = [
     url('admin/', admin.site.urls),
